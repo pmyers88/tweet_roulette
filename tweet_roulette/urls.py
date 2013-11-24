a7a57fb357d620d474e04e5f45cfe6f39ecef2cc
prm5031 <prm5031@gmail.com>
2013-11-24 13:19:59 -0500

    Enabled admin site

--- tweet_roulette/urls.py
+++ tweet_roulette/urls.py
@@ -6,13 +6,13 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-     url(r'^$', 'tweet_roulette_app.views.tweet_roulette_form', name='tweet_roulette'),
+     url(r'^$', 'tweet_roulette_app.views.tweet_roulette_form'),
      url(r'^account/$', 'tweet_roulette_app.views.create_account'),
      url(r'^account/(?P<account_id>\w{1,15})/$', 'tweet_roulette_app.views.account'),
      
      # Uncomment the admin/doc line below to enable admin documentation:
-     #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
      # Uncomment the next line to enable the admin:
-     #url(r'^admin/', include(admin.site.urls)),
+     url(r'^admin/', include(admin.site.urls)),
 )
